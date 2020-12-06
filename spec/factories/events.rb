e5918118eafccdbd77b14751748fd199c91bd2ff
soudatakeru <waito0202@gmail.com>
2020-12-06 20:59:46 +0900

    イベント投稿機能テストコード

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :event do
-    
+    association :user
+    name { 'あああ' }
+    explanation { 'あああ' }
+    facility_id { 2 }
+    scale_id { 2 }
+    category_id { 2 }
   end
 end
